f83cbc9109a214998ef100edf206adfa2843edd0
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2023-01-31 14:58:31 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div align='center'>
+            <br></br>
+            Rajesh's Foodary &copy; 2023
+            <br></br>
+            <br></br>
+            <br></br>
+        </div>
+    );
+}
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,40 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+        image: '/img/item1.jpg',
+
+    },
+    recipe_2: {
+        name: 'Dum Biryani',
+        preparation_time: '45 minutes',
+        servings: '3',
+        ingredients: [
+            "350 gm chopped chicken",
+            "20 gm garam masala powder",
+            "300 ml water",
+            "300 gm boiled basmati rice",
+            "30 ml ghee",
+            "1 cup wheat flour",
+        ],
+        instructions: [
+            'Prepare the marinade and let the chicken marinate for an hour',
+            'Cook the marinated chicken in ghee',
+            'Make the layers for biryani, add cooked basmati rice in it',
+            'Cover the pan with the dough and seal it',
+        ],
+        image: '/img/item2.jpg',
+    },
+}
